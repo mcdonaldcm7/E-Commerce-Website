ae21693735af28235f7a5d1a859c66db8f1b35a1
mcdonaldcm7 <mcdonald.cm7@gmail.com>
2024-03-21 13:39:01 +0100

    Definition of callback authenticateUser to authenticate users

---
+++ controllers/AuthControllers.js
@@ -0,0 +1,33 @@
+import bcrypt from 'bcryptjs';
+import dbClient from '../utils/db';
+
+export default async function authenticateUser(req, res) {
+  const { email, password } = req.body;
+
+  if (email === undefined) {
+    return res.status(401).json({ error: 'Missing email' });
+  }
+
+  if (password === undefined) {
+    return res.status(401).json({ error: 'Missing password' });
+  }
+
+  const db = dbClient.client.db(dbClient.database);
+  const usersCollection = db.collection('users');
+  const user = await usersCollection.findOne({ email });
+
+  if (!user) {
+    return res.status(401).json({ error: 'Incorrect email' });
+  }
+
+  try {
+    const match = bcrypt.compareSync(password, user.password);
+    if (match) {
+      return res.status(200).json({ message: 'Login successful' });
+    }
+    return res.status(401).json({ error: 'Unauthorized' });
+  } catch (err) {
+    console.error('Error comparing passwords');
+    return res.status(503).json({ error: 'Internal Server Error' });
+  }
+}
