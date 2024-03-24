3f8e490473efef0c47b505666b2018f7e52a9560
mcdonaldcm7 <mcdonald.cm7@gmail.com>
2024-03-24 20:21:06 +0100

    modifications: Added 3 new functions (getProduct, getProductQuantity, updateProductQuantity)

--- utils/db.js
+++ utils/db.js
@@ -1,4 +1,4 @@
-import { MongoClient } from 'mongodb';
+import { MongoClient, ObjectId } from 'mongodb';
 import dotenv from 'dotenv';
 
 dotenv.config();
@@ -37,6 +37,86 @@ class DBClient {
     const db = this.client.db(this.database);
     const productsCount = await db.collection('products').countDocuments();
     return productsCount;
+  }
+
+  async getProduct(name, id) {
+    const db = this.client.db(this.database);
+    const query = {};
+
+    if (!name && !id) {
+      return new Error('Insufficient query information passed');
+    }
+
+    if (name !== undefined) {
+      query.name = name;
+    }
+
+    if (id !== undefined) {
+      query._id = ObjectId(id);
+    }
+
+    try {
+      const product = await db.collection('products').findOne(query);
+      return product;
+    } catch (error) {
+      console.error(error);
+      return new Error(`getProduct: Error encountered in querying for ${query}`);
+    }
+  }
+
+  async getProductQty(name, id) {
+    const db = this.client.db(this.database);
+    const query = {};
+
+    if (!name && !id) {
+      return new Error('Insufficient query information passed');
+    }
+
+    if (name !== undefined) {
+      query.name = name;
+    }
+
+    if (id !== undefined) {
+      query._id = ObjectId(id);
+    }
+    try {
+      const product = await db.collection('products').findOne(query);
+      if (product) {
+        return product.quantity;
+      }
+      return 0;
+    } catch (error) {
+      console.error(error);
+      return new Error(`getProductQty: Error encountered in querying for ${query}`);
+    }
+  }
+
+  async updateProductQuantity(name, id, newQuantity) {
+    const db = this.client.db(this.database);
+    const productCollection = db.collection('products');
+    const query = {};
+
+    if ((!name && !id) || !newQuantity) {
+      return new Error('Insufficient query information passed');
+    }
+
+    if (name !== undefined) {
+      query.name = name;
+    }
+
+    if (id !== undefined) {
+      query._id = ObjectId(id);
+    }
+
+    try {
+      await productCollection.updateOne(query, {
+        $set: { quantity: newQuantity },
+      });
+      return { success: true, newQuantity };
+    } catch (error) {
+      console.error(error);
+      return new Error('updateProductQty: Error encountered in updating product info');
+    }
   }
 }
 
