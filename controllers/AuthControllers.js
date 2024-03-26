980c8c05daead2a7bb9c787b695040bdeeff1d0a
mcdonaldcm7 <mcdonald.cm7@gmail.com>
2024-03-26 20:53:13 +0100

    modification(s): Added definition for the function authorize

--- controllers/AuthControllers.js
+++ controllers/AuthControllers.js
@@ -2,7 +2,12 @@ import bcrypt from 'bcryptjs';
 import dbClient from '../utils/db';
 import { generateToken } from '../utils/passport';
 
-export default async function authenticateUser(req, res) {
+export async function authenticateUser(req, res, next) {
+  // Check if user is already authenticated, If so skip and call next()
+  if (req.user) {
+    return next();
+  }
+
   const { email, password } = req.body;
 
   if (email === undefined) {
@@ -25,11 +30,25 @@ export default async function authenticateUser(req, res) {
     const match = bcrypt.compareSync(password, user.password);
     if (match) {
       const token = generateToken(user);
+      const path = req.originalUrl.split('/').pop();
+      if (!['login'].includes(path)) {
+        return next();
+      }
       return res.status(200).json({ message: 'Login successful', token });
     }
-    return res.status(401).json({ error: 'Unauthorized' });
+    return res.status(401).json({ error: 'Incorrect Password' });
   } catch (err) {
     console.error('Error encountered: ', err);
     return res.status(503).json({ error: 'Internal Server Error' });
   }
+}
+
+export function authorize(role) {
+  return (req, res, next) => {
+    if (req.user && req.user.role === role) {
+      next();
+    } else {
+      res.status(403).json({ message: 'Forbidden' });
+    }
+  };
 }
