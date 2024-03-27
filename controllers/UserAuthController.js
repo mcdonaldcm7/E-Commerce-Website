0f3548c4aa2dbc8acd4ddb92b55bf04ecd7913b7
mcdonaldcm7 <mcdonald.cm7@gmail.com>
2024-03-27 16:18:02 +0100

    modification: Added comments

--- controllers/UserAuthController.js
+++ controllers/UserAuthController.js
@@ -6,6 +6,14 @@ import redisClient from '../utils/redis';
 
 dotenv.config();
 
+/*
+ * createUser - Creates a new user from the infomation supplied in the request body
+ *
+ * @req: Express request object
+ * @res: Express response object
+ *
+ * Return: Output prompt containing user ID on success or error message with reason on failure
+ */
 export async function createUser(req, res) {
   const { email, password } = req.body;
 
@@ -17,8 +25,18 @@ export async function createUser(req, res) {
     return res.status(400).json({ error: 'email must be a string' });
   }
 
+  // Regex text for the format of the email address passed
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (!emailRegex.test(email)) {
+    return res.status(400).json({ error: 'Invalid email format' });
+  }
+
   if (password === undefined) {
     return res.status(400).json({ error: 'Missing password' });
+  }
+
+  if (password.length < 14) {
+    return res.status(400).json({ error: 'Password length must be at least 14' });
   }
 
   const db = dbClient.client.db(dbClient.database);
@@ -48,6 +66,14 @@ export async function createUser(req, res) {
   }
 }
 
+/*
+ * resetPassword - Generates and returns a reset password token for the user
+ *
+ * @req: Express request object
+ * @res: Express response object
+ *
+ * Return: Output prompt containing token on success or error message on failure
+ */
 export async function resetPasswordToken(req, res) {
   const { email } = req.body;
 
@@ -55,28 +81,40 @@ export async function resetPasswordToken(req, res) {
     return res.status(400).json({ message: 'Missing email' });
   }
 
-  const db = dbClient.client.db(dbClient.database);
-  const userCollection = db.collection('users');
+  try {
+    const db = dbClient.client.db(dbClient.database);
+    const userCollection = db.collection('users');
 
-  const user = await userCollection.findOne({ email });
-  if (!user) {
-    return res.status(400).json({ error: 'User not found' });
+    const user = await userCollection.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ error: 'User not found' });
+    }
+
+    const resetToken = crypto.randomBytes(32).toString('hex');
+    await userCollection.updateOne({ email }, { $set: { resetToken } });
+
+    return res.status(200).json({ email, resetToken });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: 'Internal Server Error' });
   }
-
-  const resetToken = crypto.randomBytes(32).toString('hex');
-  await userCollection.updateOne({ email }, { $set: { resetToken } });
-
-  return res.status(200).json({ email, resetToken });
 }
 
+/*
+ * updatePassword - Validates the reset password token supplied in the json body and updates the
+ * user password
+ *
+ * @req: Express request object
+ * @res: Express response object
+ *
+ * Return: Output prompt containing token on success or error message on failure
+ */
 export async function updatePassword(req, res) {
-  const { user } = req;
+  const { email, resetToken, newPassword } = req.body;
 
-  if (!user) {
-    return res.status(401).send({ message: 'Unauthorized' });
+  if (email === undefined) {
+    return res.status(400).json({ error: 'User email not supplied' });
   }
-
-  const { resetToken, newPassword } = req.body;
 
   if (resetToken === undefined) {
     return res.status(400).json({ error: 'Reset Token not supplied!' });
@@ -88,15 +126,21 @@ export async function updatePassword(req, res) {
 
   const db = dbClient.client.db(dbClient.database);
   const userCollection = db.collection('users');
+  let user = null;
 
   try {
+    user = await userCollection.findOne({ email });
+    if (!user) {
+      return res.status(404).json({ error: 'User not found' });
+    }
+
     const match = bcrypt.compareSync(newPassword, user.password);
     if (match) {
       return res.status(400).json({ message: 'New password cannot be same as old password' });
     }
   } catch (err) {
     console.error('Error encountered in comparing passwords: ', err);
-    return res.status(503).json({ error: 'Internal Server Error' });
+    return res.status(500).json({ error: 'Internal Server Error' });
   }
 
   const salt = bcrypt.genSaltSync(10);
@@ -115,6 +159,14 @@ export async function updatePassword(req, res) {
   }
 }
 
+/*
+ * logUserOut - Nullifies the user's session token
+ *
+ * @req: Express request object
+ * @res: Express response object
+ *
+ * Return: Output prompt containing token on success or error message on failure
+ */
 export async function logUserOut(req, res) {
   const token = req.headers.authorization.split(' ')[1];
 
@@ -122,7 +174,7 @@ export async function logUserOut(req, res) {
     await redisClient.addToBlacklist(token);
   } catch (err) {
     console.log('Error encountered in adding user token to balcklist: ', err);
-    return res.status(503).json({ error: 'Internal Server Error' });
+    return res.status(500).json({ error: 'Internal Server Error' });
   }
   return res.status(301).redirect('/products');
 }
