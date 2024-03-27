2b87c582304e7f72fcb7e74ee94bb8147cce95dc
mcdonaldcm7 <mcdonald.cm7@gmail.com>
2024-03-27 13:12:20 +0100

    modification: Added comments

--- utils/verifyUserToken.js
+++ utils/verifyUserToken.js
@@ -1,5 +1,14 @@
 import redisClient from './redis';
 
+/*
+ * verifyUserToken - Checks wether the user's session token is blacklisted
+ *
+ * @req: Express request argument object
+ * @res: Express response argument object
+ * @next: Middleware to call after function completion
+ *
+ * Return: calls next
+ */
 export default async function verifyUserToken(req, res, next) {
   const token = req.headers.authorization.split(' ')[1];
 
